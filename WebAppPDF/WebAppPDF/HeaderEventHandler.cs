35f03c74d3e13c8fcf2b33e35b9454cc0bc6c8ca
paolomanolo1981 <paolo.manolo28@gmail.com>
2022-07-13 18:35:04 -0500

    c

--- WebAppPDF/WebAppPDF/HeaderEventHandler.cs
+++ WebAppPDF/WebAppPDF/HeaderEventHandler.cs
@@ -4,6 +4,7 @@ using iText.Kernel.Events;
 using iText.Kernel.Font;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
 using iText.Layout;
 using iText.Layout.Borders;
 using iText.Layout.Element;
@@ -31,14 +32,24 @@ namespace WebAppPDF
             PdfDocument pdfDoc = docEvent.GetDocument();
             PdfPage page = docEvent.GetPage();
 
+            PdfCanvas canvas1 = new PdfCanvas(page.NewContentStreamBefore(), page.GetResources(), pdfDoc);
+            Rectangle rootArea = new Rectangle(35, page.GetPageSize().GetTop() - 75, page.GetPageSize().GetRight() - 70, 55);
+            new Canvas(canvas1, pdfDoc, rootArea)
+                .Add(getTable(docEvent));
+
+
+
+
+/*
             Rectangle rootArea = new Rectangle(35, page.GetPageSize().GetTop() - 70, page.GetPageSize().GetRight() - 70, 50);
             Canvas canvas = new Canvas(docEvent.GetPage(), rootArea);
             canvas
                 .Add(getTable(docEvent))
                 .ShowTextAligned("Este es el encabezado de página", 10, 0, TextAlignment.CENTER)
                 .ShowTextAligned("Este es el pie de página", 10, 10, TextAlignment.CENTER)
+                .ShowTextAligned("texto Agregado",612,0,TextAlignment.RIGHT)
                 .Close();
-
+*/
         }
 
         public Table getTable(PdfDocumentEvent docEvent)
@@ -52,7 +63,8 @@ namespace WebAppPDF
             Style styleText =new Style()
                .SetTextAlignment(TextAlignment.RIGHT).SetFontSize(10f);
 
-            Cell cell = new Cell().Add(Img.SetAutoScale(true)).SetBorder(new SolidBorder(ColorConstants.BLACK, 1));
+            Cell cell = new Cell().Add(Img.SetAutoScale(true));
+                //.SetBorder(new SolidBorder(ColorConstants.BLACK, 0));
 
             tableEvent.AddCell(cell
                 .AddStyle(styleCell)
@@ -66,7 +78,7 @@ namespace WebAppPDF
                 .Add(new Paragraph("Departamento de Recursos Materiales").SetFont(bold))
                 .Add(new Paragraph("Fecha de Emisión: " + DateTime.Now.ToShortDateString()))
                 .AddStyle(styleText).AddStyle(styleCell)
-                .SetBorder(new SolidBorder(ColorConstants.BLACK,1))
+              //  .SetBorder(new SolidBorder(ColorConstants.BLACK,0))
                 ;
 
             tableEvent.AddCell(cell);
